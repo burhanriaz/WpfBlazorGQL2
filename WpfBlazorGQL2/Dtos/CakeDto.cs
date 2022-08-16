c4494ece9c55b309b8b0492690d9b2a702cc30e5
Burhan Riaz <burhanriaz35@gmail.com>
2022-08-16 10:51:35 +0500

    Add project files.

---
+++ WpfBlazorGQL2/Dtos/CakeDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfBlazorGQL2.Dtos
+{
+    public class CakeDto
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
